848dd64dcae0537b5fe0386eaf2b9d29b2feb093
Tahir Khan <tahir_khan54321@hotmail.com>
2025-01-07 16:47:41 +0000

    Attempt using batching

--- product_recommender/management/commands/populate_products.py
+++ product_recommender/management/commands/populate_products.py
@@ -6,31 +6,49 @@ from product_recommender.models import Product
 
 
 class Command(BaseCommand):
-    help = 'Populates the Product table with data from metadata.json'
+    help = 'Populates the Product table with data from metadata_processed.json'
 
     def handle(self, *args, **options):
         # Get the directory of the current script
-        script_dir = os.path.dirname(os.path.abspath(__file__)) 
+        script_dir = os.path.dirname(os.path.abspath(__file__))
 
         # Construct the relative path to metadata.json
         file_path = os.path.join(script_dir, '..', '..', '..', 'data_files', 'metadata_processed.json')
 
+        batch_size = 1000  # Define the desired batch size
+        data = []  # Initialize an empty list to store the batch
+
         with open(file_path, 'r') as f:
             for item in ijson.items(f, 'item'):
+                data.append(item)  # Add the item to the batch
 
-                try:
-                    # Check if the product belongs to the 'Home & Kitchen' category
-                    categories = item.get('categories', [])
-                    if any('Home & Kitchen' in category for category in categories):
-                        product = Product(
-                            product_id=item['asin'],
-                            name=item['title'],
-                            image_url=item['imUrl']
-                        )
-                        product.save()
+                if len(data) >= batch_size:
+                    self.process_batch(data)  # Process the batch
+                    data = []  # Reset the batch list
 
-                except Exception as e:  # Catch any errors during processing
-                    print(f"Error processing item: {item}")
-                    print(e)
+            # Process any remaining items after the loop
+            if data:
+                self.process_batch(data)
 
-        self.stdout.write(self.style.SUCCESS('Successfully populated the Product table'))
+    def process_batch(self, data):
+        for item in data:
+            try:
+                product_id = item['asin']
+
+                # Check if the product already exists in the database
+                if Product.objects.filter(product_id=product_id).exists():
+                    continue
+
+                # Check if the product belongs to the 'Home & Kitchen' category
+                categories = item.get('categories', [])
+                if any('Home & Kitchen' in category for category in categories):
+                    product = Product(
+                        product_id=product_id,
+                        name=item['title'],
+                        image_url=item['imUrl']
+                    )
+                    product.save()
+
+            except Exception as e:  # Catch any errors during processing
+                print(f"Error processing item: {item}")
+                print(e)
